2081d78c958dd1dc72d35899cdde290c92be6742
sehee gwon <gwonsehwi@gmail.com>
2023-05-10 04:18:29 +0900

    yjs xml 매핑 예제코드 작성 (indexedDB, xml map)

--- xml/xml.js
+++ xml/xml.js
@@ -1,26 +1,71 @@
 import * as Y from 'yjs'
-import {WebsocketProvider} from 'y-websocket'
+import {WebrtcProvider} from "y-webrtc";
+import {IndexeddbPersistence} from 'y-indexeddb'
 
-window.addEventListener('load', () => {
-  const ydoc = new Y.Doc()
-  const provider = new WebsocketProvider('wss://demos.yjs.dev', 'xml-demo', ydoc)
+const doc = new Y.Doc()
+const provider = new WebrtcProvider('my-xml-room', doc)
+new IndexeddbPersistence('my-xml-db', doc)
 
-  fetch('./sheet.xml')
-      .then(response => response.text())
-      .then((data) => {
-          // Method 1: Define a top-level type
-          const yxmlFragment = ydoc.getXmlFragment('my xml fragment')
-          // Method 2: Define Y.XmlFragment that can be included into the Yjs document
-          const yxmlElement = ydoc.get('prop-name', Y.XmlElement)
-          const yxmlNested = new Y.XmlElement('node-name')
+// xml dom
+let xmlFragment = doc.getXmlFragment('my-xml-fragment')
 
-          const yxmlText = new Y.XmlText()
-          yxmlText.insert(0, 'abc')
+const content = document.getElementById("content")
 
-          yxmlFragment.insert(0, [yxmlText])
-          yxmlFragment.insertAfter(yxmlText, [new Y.XmlElement('TEXT1')])
-          yxmlFragment.insertAfter(yxmlText, [new Y.XmlElement('TEXT2')])
+// 서버에 저장된 내용 내려받기
+fetch('./sheet.xml')
+    .then(response => response.text())
+    .then((data) => {
+        // 지정된 경로의 xml 파일 text 로 읽기
+        const xmlDoc = new DOMParser().parseFromString(data, 'text/xml')
 
-          console.log(yxmlFragment.toDOM())
-      })
+        // TEXT 요소 선택, 내용을 불러와 줄넘김한다.
+        let str = ''
+        xmlDoc.querySelectorAll('TEXT').forEach(element => {
+            str += element.innerHTML + '\n'
+        })
+
+        // textarea 에 xml 내용 추가
+        content.innerHTML = str;
+
+        console.log("서버로부터 xml 내려받기...")
+    })
+
+// action 발생 시 yjs xml 저장 (indexedDB 에 저장됨)
+// 예시는 textarea 로 작성, 입력시 이벤트 발생
+content.addEventListener("input", (e) => {
+    const value = e.target.value;
+    const txtArray = value.split('\n'); // 줄넘김 단위로 array 생성
+
+    // 줄넘김이 삭제되면 xml dom 도 삭제
+    if (txtArray.length < xmlFragment.length) {
+        xmlFragment.delete(txtArray.length-1, xmlFragment.length - txtArray.length)
+    }
+
+    // 줄넘김 단위로 TEXT 요소를 생성한다.
+    txtArray.forEach((txt, idx) => {
+        // TEXT 요소 생성
+        const xmlElement = new Y.XmlElement('TEXT')
+
+        // tid 속성 추가
+        xmlElement.setAttribute("tid", (idx+1).toString())
+        xmlElement.push([new Y.XmlText(txt)]) // textarea 입력값을 내용에 추가
+
+        // xml dom 에 존재할 경우 삭제, 아니면 추가
+        if (xmlFragment.get(idx) != null) {
+            xmlFragment.delete(idx, 1)
+        }
+        xmlFragment.insert(idx, [xmlElement])
+    })
+
+    console.log('서버에 xml 전송하기... {}', xmlFragment.toDOM())
 })
+
+// xml dom 변경사항 관찰
+/*xmlFragment.observe(xmlEvent => {
+    xmlEvent.changes.delta.forEach(e => {
+        console.log(e)
+    })
+})*/
+
+// webSocket 사용 시 클라이언트 동시 편집 가능
+// use provider
